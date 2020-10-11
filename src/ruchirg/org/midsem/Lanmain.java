ca47b38d32b0a15631e525eb05d568a7c219e585
Deep-pa <72686220+Deep-pa@users.noreply.github.com>
2020-10-11 22:04:16 +0530

    Update Lanmain.java

--- src/ruchirg/org/midsem/Lanmain.java
+++ src/ruchirg/org/midsem/Lanmain.java
@@ -2,7 +2,7 @@ package ruchirg.org.midsem;
 import ruchirg.org.midsem.Lanclass;
 
 public class Lanmain extends Lanclass{
-	public static void main(String[] args){
+	public static void main(String[] args) throws interpetedException{
 		Lanclass sp=new Lanclass();
 		sp.Lan();
 	}
